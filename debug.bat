6abec85cfacf2c8df0ee1a6fb31be89b62ae59fe
NNX <marcinjxxxx@users.noreply.github.com>
2019-06-20 22:00:58 +0200

    Increase the amout of memory in the emulated machine

--- debug.bat
+++ debug.bat
@@ -1,2 +1,2 @@
-qemu-system-x86_64  -d cpu_reset -monitor stdio -drive if=pflash,format=raw,readonly,file="OVMF_X64.fd" -drive file=fat:rw:image
+qemu-system-x86_64  -d cpu_reset -m 256 -monitor stdio -drive if=pflash,format=raw,readonly,file="OVMF_X64.fd" -drive file=fat:rw:image
 
