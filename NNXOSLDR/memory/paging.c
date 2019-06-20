fcedf1b6910d9781905bad962c3497b8f8bc12b2
NNX <marcinjxxxx@users.noreply.github.com>
2019-06-20 20:24:25 +0200

    Paging progress

--- NNXOSLDR/memory/paging.c
+++ NNXOSLDR/memory/paging.c
@@ -1,26 +1,54 @@
 #include "paging.h"
 #include "video/SimpleTextIO.h"
 
-__declspec(align(4096)) UINT64*** PML4[512];
-__declspec(align(4096)) UINT64** PML4e[512];
+__declspec(align(4096)) UINT64*** PML4[512] = {0};
+__declspec(align(4096)) UINT64** PML4e[512][512];
+__declspec(align(4096)) UINT64* temp[512] = { 0 };
+__declspec(align(4096)) UINT64 temp2[512] = { 0 };
+
+void PagingMapPage(void* virtual, void* physical, UINT16 flags) {
+	UINT64**** pml4 = GetCR3();
+	UINT64 pdp_number = (((UINT64)virtual) & PML4_ADDRESS_MASK) >> 39;
+	UINT64 pd_number = (((UINT64)virtual) & PDP_ADDRESS_MASK) >> 30;
+	UINT64 pt_number = (((UINT64)virtual) & PD_ADDRESS_MASK) >> 21;
+	UINT64 page_number = (((UINT64)virtual) & PT_ADDRESS_MASK) >> 12;
+	UINT64*** pdp = PG_ALIGN(PML4[pdp_number]);
+	UINT64** pd = PG_ALIGN(pdp[pd_number]);
+	UINT64* pt = PG_ALIGN(pd[pt_number]);
+	pt[page_number] = PG_ALIGN(physical) | flags;
+}
 
 void PagingInit() {
 	UINT64*** sourceCR3 = GetCR3();
 	UINT64** sourceFisrtEntryOfPML4 = sourceCR3[0];
 	for (int a = 0; a < 512; a++) {
-		PML4[a] = sourceCR3[a];
-		PML4e[a] = ((UINT64***)(((UINT64)PML4[0])&(~(0xfff))))[a];
+		if (!sourceCR3[a]) continue;
+		for (int b = 0; b < 512; b++) {
+			PML4e[a][b] = ((UINT64***)(((UINT64)sourceCR3[a])&(~(0xfff))))[b];
+		}
 	}
-	PML4[2] = ((UINT64)PML4e)| 0x7;
-	//PML4[0][511] = 0;
-	//PML4[7] = PML4[0];
-	PrintT("\n%x %x\n", PML4[0], PML4[1]);
+	
+	for (int a = 0; a < 512; a++) {
+		if (!sourceCR3[a]) continue;
+		PML4[a] = ((UINT64)&(PML4e[a])) | 0x7;
+	}
+
+	PML4[511] = 0;
+	
+	//BUG: for some reason modifying the paging structures set up by UEFI doesn't work.
+	//Creating new structures (all 4 levels) seems to fix the problem.
+	((UINT64*)PG_ALIGN(PML4e[0][0]))[10] = ((UINT64)temp) | 7;
+	temp[1] = ((UINT64)temp2) | 7;
+
 	SetCR3(PML4);
-	*((UINT64*)0x10000000000) = 0x839;
-	PrintT("%x\n", *((UINT64*)0));
-	//PML4[7][2] = 0;
 }
 
 void PagingTest() {
-	PrintT("Test");
+	
+	UINT64* vrt = 0x1403000;
+	UINT64* phy = 0x5000;
+	PagingMapPage(vrt, phy, PAGE_PRESENT | PAGE_WRITE | PAGE_SYSTEM);
+	*vrt = 0x4254;
+
+	PrintT("Test: v%x ? p%x\n",*vrt,*phy);
 }
--- NNXOSLDR/memory/paging.h
+++ NNXOSLDR/memory/paging.h
@@ -2,9 +2,22 @@
 #include "nnxint.h"
 
 extern UINT64*** PML4[512];
-//extern UINT64** PML4e[512];
+extern UINT64** PML4e[512][512];
 
 UINT64*** GetCR3();
 void SetCR3(UINT64***);
 void PagingTest();
 void PagingInit();
+
+#define PML4_ADDRESS_MASK 0xff8000000000
+#define PDP_ADDRESS_MASK 0x7fc0000000
+#define PD_ADDRESS_MASK 0x3fe00000
+#define PT_ADDRESS_MASK 0x1ff000
+#define PAGE_ADDRESS_MASK 0xfff
+#define PAGE_PRESENT 1
+#define PAGE_WRITE 2
+#define PAGE_USER 4
+#define PAGE_SYSTEM 0
+#define PAGE_READ 0
+#define PAGE_NOT_PRESENT 0
+#define PG_ALIGN(x) (((UINT64)x)&(~PAGE_ADDRESS_MASK))
