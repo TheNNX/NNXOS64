49887aa9d196a6e63d8632355c89de0e2f19e185
NNX <marcinjxxxx@users.noreply.github.com>
2020-07-09 16:29:10 +0200

    [NNXOSLDR.memory] Fix an annoying compilation error caused by nnxalloc.h

--- NNXOSLDR/memory/nnxalloc.h
+++ NNXOSLDR/memory/nnxalloc.h
@@ -17,21 +17,16 @@ typedef struct MemoryBlock {
 
 #ifdef __cplusplus
 extern "C" {
+#endif
 	void nnxalloc_init();
 	void nnxalloc_append(void* memblock, UINT64 sizeofMemblock);
 
 	void* nnxmalloc(UINT64 size);
 	void* nnxcalloc(UINT64 n, UINT64 size);
 	void nnxfree(void* address);
+#ifdef __cplusplus
 }
 #endif
-
-void nnxalloc_init();
-void nnxalloc_append(void* memblock, UINT64 sizeofMemblock);
-
-void* nnxmalloc(UINT64 size);
-void* nnxcalloc(UINT64 n, UINT64 size);
-void nnxfree(void* address);
 
 #endif
 
